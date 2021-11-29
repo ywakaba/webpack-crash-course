59b50ef64059531a465c35338442dc6425c38fd0
ywakaba <majesty@mta.biglobe.ne.jp>
2021-11-29 10:43:34 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import _ from 'lodash';
 
 function component() {
-  console.log('enter component');
   const element = document.createElement('div');
-  console.log('enter component2');
-  const array = ['Hello', 'webpack'];
+  const array = ['Hello', 'webpack', '!!'];
   element.innerHTML = _.join(array, ' '); 
   return element;
 }
-console.log('before append child');
 document.body.appendChild(component());
